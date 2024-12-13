996ffa4f876bfb5690e37278f496c07116d4e44d
Praseesh <150511158+praseesh@users.noreply.github.com>
2024-12-13 18:03:13 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,8 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-# from decouple import config
-# SECRET_KEY = config('secretkey')
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
