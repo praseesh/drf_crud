b81e636a00ba6c5ad3ca6789404142e3bb3dae06
Praseesh <150511158+praseesh@users.noreply.github.com>
2024-12-15 12:38:45 +0530

    Update views.py

--- users/views.py
+++ users/views.py
@@ -25,7 +25,6 @@ class LoginView(APIView):
     def post(self, request, *args, **kwargs):
         email = request.data.get('email')
         password = request.data.get('password')
-
         user = authenticate(request, email=email, password=password)
         if user is not None:
             refresh = RefreshToken.for_user(user)
